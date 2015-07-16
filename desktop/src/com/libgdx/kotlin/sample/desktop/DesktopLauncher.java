bb1af2d5a2680066069993ea3628b0ba4c4da96a
Kamei Hidetoshi <h_kamei@menue.co.jp>
2015-07-16 18:04:07 +0900

    create new LibGDX Project

---
+++ desktop/src/com/libgdx/kotlin/sample/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.libgdx.kotlin.sample.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.libgdx.kotlin.sample.KotlinGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KotlinGame(), config);
+	}
+}
